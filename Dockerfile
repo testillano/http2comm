9009d44d44c042ad3f938e4db6027d2f4eec8916
Eduardo Ramos Testillano (ert) <eduardo.ramos.testillano@gmail.com>
2021-04-01 03:21:26 +0200

    Modify build procedure and Dockerfiles
    Also, add a workflow to build and push images.
    
    Change-Id: Ibae15b3cb51ecc79fd7c85e92d1a842de324795a

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,17 @@
-ARG base_ver=latest
-FROM testillano/nghttp2_build:${base_ver}
+ARG base_tag=latest
+FROM testillano/http2comm_builder:${base_tag}
 MAINTAINER testillano
 
-LABEL testillano.http2comm_build.description="Docker image to build ert_http2comm library"
+LABEL testillano.http2comm.description="ert_http2comm library image"
 
 WORKDIR /code/build
 
 ARG make_procs=4
 ARG build_type=Release
-ARG ert_logger_ver=v1.0.7
 
-RUN apk add doxygen graphviz
-
-# ert_logger
+# ert_http2comm
+COPY . /code/build/http2comm/
 RUN set -x && \
-    wget https://github.com/testillano/logger/archive/${ert_logger_ver}.tar.gz && tar xvf ${ert_logger_ver}.tar.gz && cd logger-*/ && \
-    cmake -DERT_LOGGER_BuildExamples=OFF -DCMAKE_BUILD_TYPE=${build_type} . && make -j${make_procs} && make install && \
-    cd .. && rm -rf * && \
+    cd http2comm && cmake -DCMAKE_BUILD_TYPE=${build_type} . && make -j${make_procs} && make install && \
+    cd .. && rm -rf http2comm && \
     set +x
-
--- build.sh
+++ build.sh
@@ -3,48 +3,118 @@
 #############
 # VARIABLES #
 #############
-base_ver__dflt=latest # nghttp2_build
+image_tag__dflt=latest
+base_tag__dflt=latest
 make_procs__dflt=$(grep processor /proc/cpuinfo -c)
 build_type__dflt=Release
 ert_logger_ver__dflt=v1.0.7
 
 #############
+# FUNCTIONS #
+#############
+usage() {
+  cat << EOF
+
+  Usage: $0 [--project-image|--builder-image|--library]
+
+         --project-image: builds project image from './Dockerfile'.
+         --builder-image: builds base image from './Dockerfile.build'.
+         --library:       builds the project library using builder image.
+
+         For headless mode, prepend/export asked variables:
+
+         --project-image: image_tag, base_tag (http2comm_builder), make_procs, build_type
+         --builder-image: image_tag, base_tag (nghttp2), make_procs, build_type, ert_logger_ver
+
+         or environment variables towards docker run:
+
+         --library:       make_procs, build_type
+
+EOF
+}
+
+# $1: variable by reference; $2: default value
+_read() {
+  local -n varname=$1
+  local default=$2
+
+  local s_default="<null>"
+  [ -n "${default}" ] && s_default="${default}"
+  echo "Input '$1' value [${s_default}]:"
+
+  if [ -n "${varname}" ]
+  then
+    echo "${varname}"
+  else
+    read varname
+    [ -z "${varname}" ] && varname=${default}
+  fi
+}
+
+build_project_image() {
+  echo
+  echo "=== Build http2comm image ==="
+  echo
+  _read image_tag ${image_tag__dflt}
+  _read base_tag ${base_tag__dflt}
+  _read make_procs ${make_procs__dflt}
+  _read build_type ${build_type__dflt}
+
+  bargs="--build-arg base_tag=${base_tag}"
+  bargs+=" --build-arg make_procs=${make_procs}"
+  bargs+=" --build-arg build_type=${build_type}"
+
+  set -x
+  docker build --rm ${bargs} -t testillano/http2comm:${image_tag} . || return 1
+  set +x
+}
+
+build_builder_image() {
+  echo
+  echo "=== Build http2comm_builder image ==="
+  echo
+  _read image_tag ${image_tag__dflt}
+  _read base_tag ${base_tag__dflt}
+  _read make_procs ${make_procs__dflt}
+  _read build_type ${build_type__dflt}
+  _read ert_logger_ver ${ert_logger_ver__dflt}
+
+  bargs="--build-arg base_tag=${base_tag}"
+  bargs+=" --build-arg make_procs=${make_procs}"
+  bargs+=" --build-arg build_type=${build_type}"
+  bargs+=" --build-arg ert_logger_ver=${ert_logger_ver}"
+
+  set -x
+  docker build --rm ${bargs} -f Dockerfile.build -t testillano/http2comm_builder:${image_tag} . || return 1
+  set +x
+}
+
+build_library() {
+  echo
+  echo "=== Build http2comm library ==="
+  echo
+  _read make_procs ${make_procs__dflt}
+  _read build_type ${build_type__dflt}
+
+  rm -f CMakeCache.txt
+  envs="-e MAKE_PROCS=${make_procs} -e BUILD_TYPE=${build_type}"
+
+  set -x
+  docker run --rm -it -u $(id -u):$(id -g) ${envs} -v ${PWD}:/code -w /code testillano/http2comm_builder || return 1
+  docker run --rm -it -u $(id -u):$(id -g) ${envs} -v ${PWD}:/code -w /code testillano/http2comm_builder "" doc || return 1
+  set +x
+}
+#############
 # EXECUTION #
 #############
 cd $(dirname $0)
-echo
-echo "---------------------"
-echo "Build 'builder image'"
-echo "---------------------"
-echo
-echo "Input 'make_procs' [${make_procs__dflt}]:"
-read make_procs
-[ -z "${make_procs}" ] && make_procs=${make_procs__dflt}
-bargs="--build-arg make_procs=${make_procs}"
 
-echo "Input nghttp2_build 'base_ver' [${base_ver__dflt}]:"
-read base_ver
-[ -z "${base_ver}" ] && base_ver=${base_ver__dflt}
-bargs+=" --build-arg base_ver=${base_ver}"
+case "$1" in
+  --project-image) build_project_image ;;
+  --builder-image) build_builder_image ;;
+  --library) build_library ;;
+  *) usage && exit 1 ;;
+esac
 
-echo "Input ert_logger version [${ert_logger_ver__dflt}]:"
-read ert_logger_ver
-[ -z "${ert_logger_ver}" ] && ert_logger_ver=${ert_logger_ver__dflt}
-bargs+=" --build-arg ert_logger_ver=${ert_logger_ver}"
-
-docker build --rm ${bargs} -t testillano/http2comm_build . || exit 1
-
-echo
-echo "-------------"
-echo "Build project"
-echo "-------------"
-echo
-echo "Input build type (Release|Debug) [${build_type__dflt}]:"
-read build_type
-[ -z "${build_type}" ] && build_type=${build_type__dflt}
-
-rm -f CMakeCache.txt
-envs="-e MAKE_PROCS=${make_procs} -e BUILD_TYPE=${build_type}"
-docker run --rm -it -u $(id -u):$(id -g) ${envs} -v ${PWD}:/code -w /code testillano/http2comm_build || exit 1
-docker run --rm -it -u $(id -u):$(id -g) ${envs} -v ${PWD}:/code -w /code testillano/http2comm_build "" doc
+exit $?
 
