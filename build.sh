e0270de84d7ef2ba64bd6940aa2fed6c31458881
Eduardo Ramos Testillano (ert) <eduardo.ramos.testillano@gmail.com>
2023-04-21 01:25:33 +0200

    Update metrics library

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ ert_logger_ver__dflt=v1.0.10
 jupp0r_prometheuscpp_ver__dflt=v0.13.0
 # 3rd party used by prometheus:
 civetweb_civetweb_ver__dflt=v1.14
-ert_metrics_ver__dflt=v1.0.1
+ert_metrics_ver__dflt=v1.0.2
 registry=ghcr.io/testillano
 
 #############
