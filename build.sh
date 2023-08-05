8c81ee2ad3a18e8661b128ac5211ed7f68c1f4a8
Eduardo Ramos Testillano (eramedu) <eduardo.ramos.testillano@ericsson.com>
2023-08-05 12:43:42 +0200

    Upgrade metrics library

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ ert_queuedispatcher_ver__dflt=v1.0.3
 jupp0r_prometheuscpp_ver__dflt=v0.13.0
 # 3rd party used by prometheus:
 civetweb_civetweb_ver__dflt=v1.14
-ert_metrics_ver__dflt=v1.0.3
+ert_metrics_ver__dflt=v1.0.4
 registry=ghcr.io/testillano
 
 #############
