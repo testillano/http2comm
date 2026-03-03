63929ec438788d16ddd34d9b629652387412b07d
Eduardo Ramos Testillano (ert) <eduardo.ramos.testillano@gmail.com>
2026-03-03 03:50:44 +0100

    Update HTTP Codes

--- include/ert/http2comm/Http.hpp
+++ include/ert/http2comm/Http.hpp
@@ -51,22 +51,50 @@ class ResponseCode
 public:
     enum Value
     {
+        // 1xx Informational
+        CONTINUE = 100,
+        SWITCHING_PROTOCOLS = 101,
+
+        // 2xx Success
         OK = 200,
         CREATED = 201,
         ACCEPTED = 202,
         NO_CONTENT = 204,
+        PARTIAL_CONTENT = 206,
+
+        // 3xx Redirection
         MULTIPLE_CHOICES = 300,
+        MOVED_PERMANENTLY = 301,
+        FOUND = 302,
+        SEE_OTHER = 303,
+        NOT_MODIFIED = 304,
+        TEMPORARY_REDIRECT = 307,
+        PERMANENT_REDIRECT = 308,
+
+        // 4xx Client Error
         BAD_REQUEST = 400,
         UNAUTHORIZED = 401,
+        FORBIDDEN = 403,
         NOT_FOUND = 404,
         METHOD_NOT_ALLOWED = 405,
+        NOT_ACCEPTABLE = 406,
+        REQUEST_TIMEOUT = 408,
+        CONFLICT = 409,
+        GONE = 410,
         LENGTH_REQUIRED = 411,
+        PRECONDITION_FAILED = 412,
+        PAYLOAD_TOO_LARGE = 413,
         URI_TOO_LONG = 414,
         UNSUPPORTED_MEDIA_TYPE = 415,
         UNPROCESSABLE = 422,
+        TOO_MANY_REQUESTS = 429,
+
+        // 5xx Server Error
         INTERNAL_SERVER_ERROR = 500,
         NOT_IMPLEMENTED = 501,
-        SERVICE_UNAVAILABLE = 503
+        BAD_GATEWAY = 502,
+        SERVICE_UNAVAILABLE = 503,
+        GATEWAY_TIMEOUT = 504
     };
 };
 
